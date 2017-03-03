ebc567929a86e697b27ff4274a7cf85b07301342
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-03-03 15:17:06 +0300

    queries

---
+++ Cmas.Backend.Modules.Contracts.Datalayer.Couchdb/Queries/FindByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using Cmas.Backend.Infrastructure.Domain.Queries;
+using Cmas.Backend.Modules.Contracts.Criteria;
+using Cmas.Backend.Modules.Contracts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cmas.Backend.Modules.Contracts.Datalayer.Couchdb.Queries
+{
+    public class FindByIdQuery : IQuery<FindById, Contract>
+    {
+        public Contract Ask(FindById criterion)
+        {
+            return new Contract{ Name = criterion.Id.ToString()};
+        }
+    }
+}
